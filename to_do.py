742d5a7908d6e664bee5f0f2690d001424e28e5e
Ilia Samodin <broobooot@gmail.com>
2023-07-21 19:45:18 +0500

    add views to handle basic http-exceptions

--- to_do.py
+++ to_do.py
@@ -315,5 +315,29 @@ def page_not_found(exception):
     return render_template("error_page.html", page_title=page_title, error=error), error
 
 
+@app.errorhandler(500)
+def server_error(exception):
+    page_title = "Error"
+    error = 500
+
+    return render_template("error_page.html", page_title=page_title, error=error), error
+
+
+@app.errorhandler(403)
+def permission_denied(exception):
+    page_title = "Error"
+    error = 403
+
+    return render_template("error_page.html", page_title=page_title, error=error), error
+
+
+@app.errorhandler(400)
+def bad_request(exception):
+    page_title = "Error"
+    error = 400
+
+    return render_template("error_page.html", page_title=page_title, error=error), error
+
+
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
